bb0fe2e8c19ebd90ac0387d18131b1c167be6ea6
Alan Horne <alan.horne@gmail.com>
2023-07-24 02:59:35 -0500

    Added Benchmarks project

---
+++ Benchmarks/Benchmarks.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using Microsoft.FSharp.Collections;
+using Microsoft.FSharp.Core;
+using Perfolizer.Horology;
+using static FSharpCollections.Examples.Pervasives;
+
+namespace Benchmarks
+{
+    public class FastAndDirtyConfig : ManualConfig
+    {
+        public FastAndDirtyConfig()
+        {
+            var fastJob =
+                Job.Default
+                    .WithLaunchCount(1) // benchmark process will be launched only once
+                    .WithIterationTime(TimeInterval.FromMilliseconds(100)) // 100ms per iteration
+                    .WithWarmupCount(3) // 3 warmup iteration
+                    .WithIterationCount(3); // 3 target iteration
+
+            AddJob(fastJob);
+        }
+    }
+
+    [Config(typeof(FastAndDirtyConfig))]
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void Scenario1()
+        {
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+        }
+    }
+    
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
