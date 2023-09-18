ce850082b6dc414a25132ff2b3e400578fb22142
Alan Horne <ahorne@retrostudios.com>
2023-09-18 14:43:29 -0500

    Updated FSharpList  examples

--- FSharpCollections/Examples/FSharpList.cs
+++ FSharpCollections/Examples/FSharpList.cs
@@ -49,10 +49,55 @@ public static class ForFSharpList
         FSharpList<int> smallerLargeList = largeList.Tail;
         Console.WriteLine($"{smallerLargeList.Length:n0}");
         
-        // var twoLits = ListModule.Partition()
         
-
-        Console.WriteLine(emptyList);
-        Console.WriteLine();
+        // C# / F# function interop can be a bit awkward - extension methods can help
+        FSharpList<int> all = ListModule.Truncate(1_000, largeList); // 0 to 999
+        (FSharpList<int> even, FSharpList<int> odd) = ListModule.Partition(FuncConvert.FromFunc<int, bool>(n => n % 2 == 0), all); // 0 to 999 evens, 0 to 999 odds
+        
+        
+        // Choose is useful - can replace patterns like .Where(x => convertAndTest(x)).Select(x => convert(x)) in one step
+        // It requires use of another F# type - Option
+        FSharpList<int?> mixedNullity = ListModule.Initialize(1_000, FuncConvert.FromFunc<int, int?>(ord => ord % 2 == 0 ? ord : null)); // 0 to 999,999
+        FSharpList<int> nonNulls =
+            ListModule.Choose(
+                FuncConvert.FromFunc<int?, FSharpOption<int>>(nullableInt => 
+                    OptionModule.Map(
+                        FuncConvert.FromFunc<int, int>(n => n * n),
+                        OptionModule.OfNullable(nullableInt))),
+                mixedNullity);
     }
+}
+
+public static class FSharpExtensions
+{
+    public static FSharpList<U> Choose<T, U>(this FSharpList<T> list, Func<T, FSharpOption<U>> f) =>
+        ListModule.Choose(FuncConvert.FromFunc(f), list);
+
+    public static FSharpOption<U> Map<T, U>(this FSharpOption<T> option, Func<T, U> f) =>
+        OptionModule.Map(FuncConvert.FromFunc(f), option);
+
+    public static FSharpOption<T> ToOption<T>(this T? value) where T : struct =>
+        OptionModule.OfNullable(value);
+
+    public static FSharpOption<T> ToOption<T>(this T value) where T : class =>
+        OptionModule.OfObj(value);
+}
+
+public static class FSharpList
+{
+    public static FSharpList<T> Initialize<T>(int count, Func<int, T> f) =>
+        ListModule.Initialize(count, FuncConvert.FromFunc(f));
+}
+
+public static class ForFSharpList2
+{
+    public static void Run()
+    {
+        FSharpOption<int> opt;
+        
+        // The last example above is very explicit and ugly, let's clean it up some
+        FSharpList<int?> mixedNullity = FSharpList.Initialize(1_000, ord => ord % 2 == 0 ? (int?)ord : null); // 0 to 999,999
+        FSharpList<int> nonNulls = mixedNullity.Choose(nullableInt => nullableInt.ToOption().Map(n => n * n));
+    } 
+    
 }
